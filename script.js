889cbc70fdc8ccc1a5529295426619390751b54d
jeffreyadamo <jeffreyadamo@gmail.com>
2020-12-08 23:10:10 -0800

    2 way unit conversion. break lines clean it up

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// const tempF = document.getElementById("tempF").value;
-
 $("#tempForm").on("submit", function(event) {
     event.preventDefault();
-    const tempF = document.getElementById("tempF").value;
-    console.log(tempF);
+    let tempF = document.getElementById("tempF").value;
     let tempC = (tempF - 32) * (5/9);
-    $("#tempC").html(tempC.toFixed(2) + " &#176" + "C");
+    document.getElementById("tempC").value = tempC.toFixed(2);
+})
+
+$("#tempCForm").on("submit", function(event) {
+    event.preventDefault();
+    let tempC = document.getElementById("tempC").value;
+    let tempF = ((tempC * (9/5)) + 32);
+    document.getElementById("tempF").value = tempF.toFixed(2);
 })
 
