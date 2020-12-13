01b7c29c73cefe2741ff6ef27700fa8807273d90
jeffreyadamo <jeffreyadamo@gmail.com>
2020-12-12 16:54:13 -0800

    party mode switch toggles background animation

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
-$("#tempForm").on("submit", function(event) {
-    event.preventDefault();
-    let tempF = document.getElementById("tempF").value;
-    let tempC = (tempF - 32) * (5/9);
-    document.getElementById("tempC").value = tempC.toFixed(2);
-})
+$(document).ready(function(){
 
-$("#tempCForm").on("submit", function(event) {
-    event.preventDefault();
-    let tempC = document.getElementById("tempC").value;
-    let tempF = ((tempC * (9/5)) + 32);
-    document.getElementById("tempF").value = tempF.toFixed(2);
-})
 
+    $("#tempForm").on("submit", function(event) {
+        event.preventDefault();
+        let tempF = document.getElementById("tempF").value;
+        let tempC = (tempF - 32) * (5/9);
+        document.getElementById("tempC").value = tempC.toFixed(2);
+    })
+    
+    $("#tempCForm").on("submit", function(event) {
+        event.preventDefault();
+        let tempC = document.getElementById("tempC").value;
+        let tempF = ((tempC * (9/5)) + 32);
+        document.getElementById("tempF").value = tempF.toFixed(2);
+    })
+    
+    // Party Mode
+    let partyMode = false;
+    $("#partySwitch").on("change", function(event) {
+        // event.preventDefault();
+        if (!partyMode){
+            partyMode = true; 
+            // let partySection = document.createElement("section");
+            // $("#partyMode").append(partySection);
+            $("#partyMode").append("<section></section>");
+            setInterval(createSquare, 150);
+        } else {
+            partyMode = false;
+            clearInterval(createSquare);
+            $("#partyMode").empty();
+
+            
+        }
+        console.log("partyMode is " + partyMode);
+    })
+
+    // Animation Squares
+    ///////////////////////
+    const colors = [
+        '#2196f3',
+        '#e91e63',
+        '#ffeb3b',
+        '#74ff1d',
+    ]
+    function createSquare() {
+        const section = document.querySelector("section");
+        const square = document.createElement("span");
+        var size = Math.random() * 50;
+        square.style.width = 20 + size + 'px';
+        square.style.height = 20 + size + 'px';
+        square.style.top = Math.random() * innerHeight + 'px';
+        square.style.left = Math.random() * innerWidth + 'px';
+        const bg = colors[Math.floor(Math.random() * colors.length)];
+        square.style.background = bg;
+        if(partyMode){
+            section.appendChild(square);
+        };
+        setTimeout(() =>{
+            square.remove()
+        },5000)
+    }
+    
+    
+    ///////////////////////
+
+})
