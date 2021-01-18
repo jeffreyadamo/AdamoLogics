e61efae6e0b59653ea1a6113eaa2f42fdf62c054
jeffreyadamo <jeffreyadamo@gmail.com>
2021-01-17 17:16:56 -0800

    responsive font resize for calc

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ require("dotenv").config();
 // Sets up the Express App
 // =============================================================
 const app = express();
+const morgan = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.static(__dirname + "/"));
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(compression());
+app.use(helmet());
 app.use(express.json());
 
 require("./routes/api-routes.js")(app);
@@ -24,7 +30,7 @@ app.get("*", (req, res) => {
 res.sendFile(path.join(__dirname, "index.html"));
 });
 
-  // Starts the server to begin listening
+// Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
